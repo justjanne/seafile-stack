401048cf5c5715cfc5fded5f46d8d9ecbd9f13cb
Janne Mareike Koschinski <mail@justjanne.de>
2026-04-13 11:48:51 +0200

    Use inner service url between office and seahub

--- config/seahub_settings.py
+++ config/seahub_settings.py
@@ -34,12 +34,17 @@ CACHES = {
 
 FILE_SERVER_ROOT = 'http://localhost:8082'
 INNER_FILE_SERVER_ROOT = 'http://seafile:8082'
-SEAFEVENTS_SERVER_URL = 'http://seafevents:8889'
+
 SERVICE_URL = 'http://localhost:8000'
+INNER_SERVICE_URL = 'http://seahub:8000'
+
+ONLYOFFICE_ROOT = "http://localhost:8080"
+INNER_ONLYOFFICE_ROOT = "http://office"
 
 ENABLE_ONLYOFFICE = True
 VERIFY_ONLYOFFICE_CERTIFICATE = False
 ONLYOFFICE_APIJS_URL = 'http://localhost:8080/web-apps/apps/api/documents/api.js'
+ONLYOFFICE_CONVERTER_URL = 'http://office/ConvertService.ashx'
 ONLYOFFICE_FILE_EXTENSION = ('doc', 'docx', 'ppt', 'pptx', 'xls', 'xlsx', 'odt', 'fodt', 'odp', 'fodp', 'ods', 'fods')
 ONLYOFFICE_EDIT_FILE_EXTENSION = ('docx', 'pptx', 'xlsx')
 
